c228170e64dd66593fcd4ac23e9c71c4f4d494b3
bopboy <39391891+bopboy@users.noreply.github.com>
2023-12-26 14:41:39 +0900

    스타필드 애니메이션 구현

--- starfield/star.js
+++ starfield/star.js
@@ -1,11 +1,8 @@
 export default class Star {
-  constructor(number, x, y, scale) {
-    const element = document.createElement("div");
-    element.classList.add("star");
-    element.innerHTML = number;
-    element.style.left = `${x}px`;
-    element.style.top = `${y}px`;
-    element.style.transform = `scale(${scale})`;
-    document.body.append(element);
+  constructor(number) {
+    this.element = document.createElement("div");
+    this.element.classList.add("star");
+    this.element.innerHTML = number;
+    document.body.append(this.element);
   }
 }
